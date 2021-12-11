daa4cb37c9d0993bd6f43a8e3b11596835600a0a
Zhe-Ming, Zhang <48936051+qaz9517532846@users.noreply.github.com>
2021-12-11 10:42:53 +0800

    Add files via upload

--- zm_robot_programing/src/zm_robot_move.cpp
+++ zm_robot_programing/src/zm_robot_move.cpp
@@ -1,24 +1,18 @@
 #include <boost/thread.hpp>
 #include <zm_robot_programing/zm_robot_move_function.h>
 
-void spinThread(){
-  ros::spin();
-}
-
 int main(int argc, char** argv) 
 {
   ros::init(argc, argv, "zm_robot_move"); 
   ros::NodeHandle n;
 
-  boost::thread spin_thread = boost::thread(boost::bind(&spinThread));
-
   zm_robot my_zm_robot;
 
   ////// zm_robot programing control start //////
 
-  my_zm_robot.move_map(1.0, 3.0, 1.5708);
+  my_zm_robot.move_map(-1.0, -1.0, 1.5708);
   
-  my_zm_robot.move_base(1.0, 3.0, 1.5708);
+  my_zm_robot.move_base(-1.0, -1.0, 1.5708);
 
   ////// END //////
 
